7dd5c331f0649898d22f9a1ef0bfe48fa59a3395
Ваше Имя <theterorrplaygt@gmail.com>
2026-05-28 23:23:16 +0300

    Модуль 2: добавлен игровой класс в Character  с ветвлением формул

--- character.py
+++ character.py
@@ -4,11 +4,11 @@ from unit import Unit
 class Character(Unit):
     def __init__(self, strength: int, dexterity: int, constitution: int,
                  wisdom: int, intelligence: int, charisma: int,
-                 character_class: str):
+                 character_class: str) -> None:
         super().__init__(strength, dexterity, constitution,
                          wisdom, intelligence, charisma)
         if character_class not in ('warrior', 'mage', 'hunter'):
-            raise ValueError("character_class must be 'warrior', 'mage' or 'hunter'")
+            raise ValueError(f"Недопустмый класс персонажа: {character_class}")
         self.character_class = character_class
         self.max_health = self.calculate_max_health()
         self.current_health = self.max_health
@@ -21,15 +21,17 @@ class Character(Unit):
     def calculate_damage(self) -> int:
         if self.character_class == 'warrior':
             return int(self.strength * 2.2 + self.constitution // 3)
-        elif self.character_class == 'mage':
+        if self.character_class == 'mage':
             return int(self.intelligence * 2.5 + self.wisdom // 2)
-        else:  # hunter
+        if self.character_class == 'hunter':
             return int(self.dexterity * 1.9 + self.strength // 3)
+        return 0
 
     def calculate_defense(self) -> int:
         if self.character_class == 'warrior':
             return int(self.constitution * 1.8 + self.strength // 4)
-        elif self.character_class == 'mage':
+        if self.character_class == 'mage':
             return int(self.wisdom * 1.3 + self.intelligence // 6)
-        else:  # hunter
+        if self.character_class == 'hunter':
             return int(self.dexterity * 1.6 + self.constitution // 5)
+        return 0
